ee122ea0d9299fa99a35567c8d2fe018e623fc93
Chris Pachomski <cpachomski@gmail.com>
2018-11-23 18:49:14 -0500

    import theme system from hn-clone

---
+++ src/design/detail.js
@@ -0,0 +1,84 @@
+import { keyframes } from 'styled-components';
+
+const animation = {
+  fadeInDown: keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`
+};
+
+const curves = {
+  cubic: 'cubic-bezier(0.785, 0.135, 0.15, 0.86)'
+};
+
+const durations = {
+  short: '0.3s',
+  medium: '1s'
+};
+
+const delays = {
+  short: '0.04s'
+};
+
+const fonts = {
+  sansSerif: 'sans-serif'
+};
+
+export const colors = {
+  beige: '#f6f6ef',
+  black: '#000000',
+  gray: '#828282',
+  orange: '#ff6600',
+  white: '#ffffff'
+};
+
+export const themes = {
+  light: {
+    name: 'light',
+    animation,
+    colors: {
+      bkg: colors.white,
+      main: colors.black,
+      loud: colors.orange,
+      quiet: colors.gray
+    },
+    curves,
+    delays,
+    durations,
+    text: {
+      fonts,
+      size: {
+        small: '1em',
+        medium: '1.2em',
+        large: '2.3em',
+        extraLarge: '5em'
+      }
+    }
+  },
+  dark: {
+    name: 'dark',
+    animation,
+    colors: {
+      bkg: colors.black,
+      main: colors.white,
+      loud: colors.orange,
+      quiet: colors.gray
+    },
+    curves,
+    delays,
+    durations,
+    text: {
+      fonts,
+      size: {
+        small: '1em',
+        medium: '1.2em',
+        large: '2.3em',
+        extraLarge: '5em'
+      }
+    }
+  }
+};
---
+++ src/design/structure.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const xContainer = styled.div`
+  max-width: 768px;
+  margin: 0 auto;
+  overflow: hidden;
+  padding: 0px 10px;
+`;
