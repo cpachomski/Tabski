af51d98e8505e28ea89cc04be21eb78540bea2dc
Chris Pachomski <cpachomski@gmail.com>
2018-11-24 14:20:05 -0500

    refactor sheet reducer to fit style of other reducers

--- src/reducers/sheet.js
+++ src/reducers/sheet.js
@@ -1,8 +1,11 @@
 import update from 'immutability-helper';
-import { SET_SHEET_NAME } from 'actions';
+import { SET_SHEET_NAME, SET_SHEET_STATE } from 'actions';
 
 export default {
-  [SET_SHEET_NAME]: (state, action) =>
-    update(state, { sheetName: { $set: action.sheetName } }),
+  [SET_SHEET_NAME]: (state, action) => {
+    const { sheetName } = action;
+
+    return update(state, { sheetName: { $set: sheetName } });
+  },
   undoableActions: [SET_SHEET_NAME]
 };
