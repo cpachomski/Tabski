4372c4372260a791e3993aa50f5f27eb73994692
Chris Pachomski <cpachomski@gmail.com>
2018-11-24 14:21:55 -0500

    add sheet to app root

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import configureStore from 'store';
 
-import Note from 'organisms/note';
+import Sheet from 'organisms/sheet';
 
 class App extends Component {
   render() {
     return (
       <Provider store={configureStore()}>
-        <Note />
+        <Sheet />
       </Provider>
     );
   }
