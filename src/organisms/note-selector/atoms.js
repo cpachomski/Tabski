5648e6b2cf68c94283e364a3fc6b804f2e5ff6b6
Chris Pachomski <cpachomski@gmail.com>
2019-01-13 13:00:12 -0500

    move note selector frettings to config, update proptypes and set unique key

---
+++ src/organisms/note-selector/atoms.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.ul`
+  list-style: none;
+`;
--- src/organisms/note-selector/index.js
+++ src/organisms/note-selector/index.js
@@ -1,1 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { frettings } from "./config";
+
+import { Wrapper } from "./atoms";
+
+function NoteSelector({ sectionId, chordId, noteId }) {
+  return (
+    <Wrapper>
+      {frettings.map(fretting => (
+        <li key={`s:${sectionId}-c:${chordId}-n:${noteId}-f:${fretting}`}>
+          {fretting}
+        </li>
+      ))}
+    </Wrapper>
+  );
+}
+
+NoteSelector.propTypes = {
+  sectionId: PropTypes.number,
+  chordId: PropTypes.number,
+  noteId: PropTypes.number
+};
+
+export default NoteSelector;
