aed6eaa53f5e585002bf56122db910f54a38aaf0
Chris Pachomski <cpachomski@gmail.com>
2019-01-13 20:46:45 -0500

    remove react-pie-menu and remove theme dependency provider

--- src/app.js
+++ src/app.js
@@ -1,45 +1,15 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
-import { ThemeProvider, css } from "styled-components";
 import configureStore from "store";
 
 import Sheet from "organisms/sheet";
 
-const theme = {
-  pieMenu: {
-    container: css``,
-    list: css`
-      background-color: #000;
-    `,
-    item: css`
-      background-color: #000;
-    `,
-    center: css`
-      background: #333;
-    `
-  },
-  slice: {
-    container: css`
-      background-color: #000;
-      color: white;
-      &:hover {
-        background-color: #333;
-      }
-    `,
-    content: css`
-      font-size: 30px;
-    `
-  }
-};
-
 class App extends Component {
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <Provider store={configureStore()}>
-          <Sheet />
-        </Provider>
-      </ThemeProvider>
+      <Provider store={configureStore()}>
+        <Sheet />
+      </Provider>
     );
   }
 }
